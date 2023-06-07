08828f6cef4e785f7e3bb7db6cd353c342853928
Hüseyin Alp Altınok <102041715+AlpAltinok@users.noreply.github.com>
2023-06-07 21:24:53 +0300

    Camera system

---
+++ Assets/Script/CameraFlow.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlow : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float speed = 10f;
+    public Vector3 dist;
+    public Transform lookTarget;
+
+    private void LateUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, speed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
